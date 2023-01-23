1d2533850a006373e5c121b0a5e319f002165e5c
Radhika403 <radhikabansal403@gmail.com>
2023-01-24 01:31:45 +0530

    Trying things out. WIP

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
+// console.log(proces s.env.CLIENT_ID)
+
 app.get('/', (req, res) =>{
     res.send("hi");
 });
@@ -49,8 +51,8 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/callback', (req, res) => {
-    const code = req.query.code || null;
-  
+    const code = req.query.code || null; // authorisation code to geenrate token
+    
     axios({
       method: 'post',
       url: 'https://accounts.spotify.com/api/token',
@@ -80,6 +82,15 @@ app.get('/callback', (req, res) => {
             .catch(error => {
               console.log(error);
             });
+
+          // axios.get(`http://localhost:8888/refresh_token?refresh_token=${refresh_token}`) 
+          //   .then(response => {
+          //     res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
+          //   })
+          //   .catch(error => {
+          //     console.log(error);
+          //   });
+          
     
         } else {
           res.send(response);
@@ -90,6 +101,29 @@ app.get('/callback', (req, res) => {
       });
   });
 
+  // app.get('/refresh_token', (req, res) => {
+  //   const { refresh_token } = req.query;
+  
+  //   axios({
+  //     method: 'post',
+  //     url: 'https://accounts.spotify.com/api/token',
+  //     data: querystring.stringify({
+  //       grant_type: 'refresh_token',
+  //       refresh_token: refresh_token
+  //     }),
+  //     headers: {
+  //       'content-type': 'application/x-www-form-urlencoded',
+  //       Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+  //     },
+  //   })
+  //     .then(response => {
+  //       res.send(response.data);
+  //     })
+  //     .catch(error => {
+  //       res.send(error);
+  //     });
+  // });
+
 app.listen(port, () => {
-    console.log("Express is listening at 8888");
+    console.log(`Express is listening at http://localhost:${port}`);
 })
